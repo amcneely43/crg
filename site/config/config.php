daa5607c157489b3742b01b21a2ef4a064141d23
Andrew McNeely <amcneely43@gmail.com>
2026-05-10 20:31:09 -0700

    Fix Kirby route pattern: concepts (not api/concepts)

--- site/config/config.php
+++ site/config/config.php
@@ -8,7 +8,7 @@ return [
   'smartypants' => true,
   'routes' => [
     [
-      'pattern' => 'api/concepts',
+      'pattern' => 'concepts',
       'action'  => function () {
         $term = trim(get('term', ''));
 
